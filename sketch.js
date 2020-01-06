56140db44da8e32ca57e0d3f309b26d269dc4a48
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2020-01-06 16:34:15 +0100

    project 6/01

--- sketch.js
+++ sketch.js
@@ -1,11 +1,70 @@
-function preload(){
-  // put preload code here
+var trex;
+var glitter;
+var button;
+var song;
+
+
+function preload() {
+  trex = loadModel("./assets/T-Rex Model.obj", true);
+  glitter = loadImage("./assets/giphyrosa.gif");
+  song = loadSound("./assets/ProfondoRosso.mp3");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+
+  //song
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(song);
+  //song volume
+  volume = analyzer.getLevel();
+  volume = map(volume, 0, 1, 0, height);
+
+  button = createButton("RIDDICULUS");
+  button.size(100, 20);
+  button.position((width / 2) - 50, (height / 2) + 300);
+  button.mousePressed(clickButton);
+
+  //song
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(song);
+
+
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+
+  //istruzioni create con un div
+  guide = createDiv('click the button to play<br>(click everywhere to play/pause)');
+  guide.style('position: absolute; width: 25vw; height: 10vh; bottom: 0.5%; left: 50%; transform: translate(-50%); color: white; text-align: center; font-family: Poppins; font-size: 0.7vw');
+
+
+  orbitControl(12, 1, 0.1);
+  rotateZ(180);
+
+  rotateY(frameCount + volume / 2000);
+  noStroke();
+
+  ambientLight(40, 0, 0);
+  var mouse1 = mouseX - height / 2;
+  var mouse2 = mouseY - width / 2;
+
+  directionalLight(155, 20, 0, 1, 0, 0);
+  pointLight(0, 0, 155, mouse1, mouse2, 0);
+
+  ambientMaterial(255);
+  model(trex);
+}
+
+function clickButton() {
+  window.open("index2.html", "_self");}
+
+//Pause and Play on click
+function mousePressed() {
+  if(!song.isPlaying()){  song.play();  } else{  song.pause();  }}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
---
+++ sketch2.js
@@ -0,0 +1,106 @@
+var trex;
+var glitter;
+var button;
+var song1;
+var analyzer;
+var trexs = [];
+
+function preload() {
+  trex = loadModel("./assets/T-Rex Model.obj", true);
+  glitter = loadImage("./assets/giphyrosa.gif");
+  song1 = loadSound("./assets/AllIWantFor.mp3");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+
+
+  //song
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(song1);
+  //song volume
+  volume = analyzer.getLevel();
+  volume = map(volume, 0, 1, 0, height);
+
+
+  push();
+  button = createButton("BE AFRAID TO CLICK");
+  button.size(160, 20);
+  button.position((windowWidth / 2) - 80, (height / 2) + 300);
+  button.mousePressed(clickButton);
+  // moodColor = color(random() * 255, random() * 0, random() * 0, 50);
+  pop();
+
+  for (var i = 0; i < 100; i++) {
+    let p = new Trex(createVector(random(-width, width, ), random(-1920, 1920), random(100, -100)), random(50, 30), random(1, 10), random(0.05));
+    trexs.push(p);
+  }
+
+}
+
+function draw() {
+  background(0);
+
+  //istruzioni create con un div
+  guide = createDiv('click the button to play<br>(click everywhere to play/pause)');
+  guide.style('position: absolute; width: 25vw; height: 10vh; bottom: 0.5%; left: 50%; transform: translate(-50%); color: white; text-align: center; font-family: Poppins; font-size: 0.7vw');
+
+  orbitControl(12, 1, 0.1);
+  trexs.forEach(x => x.update());
+  trexs.forEach(x => x.render());
+
+
+}
+
+//button for going back to the scary trex
+function clickButton() {
+  window.open("index.html", "_self");
+}
+
+//inserisco il modello e lo faccio scendere a pioggia
+class Trex {
+
+  constructor(position, size, speed, rotateSpeed) {
+    this.pos = position;
+    this.size = size;
+    this.speed = speed;
+    this.rotateSpeed = rotateSpeed;
+  }
+
+  update() {
+    this.pos.y += this.speed;
+    if (this.pos.y > height * 2 + this.size) {
+      this.pos.y = 0 - height * 2 + this.size;
+    }
+  }
+
+  render() {
+    push();
+    translate(this.pos.x, this.pos.y, this.pos.z);
+    rotate(frameCount * this.rotateSpeed)
+    rotateY(frameCount * this.rotateSpeed / 1.5);
+    rotateZ(frameCount * this.rotateSpeed / 0.5);
+    push();
+    noStroke();
+    ambientLight(255, 255, 255);
+    directionalLight(255, 255, 255, -1, 0, 0);
+    texture(glitter);
+    rotateZ(180);
+    model(trex, (this.size / 2), (this.size / 6), 20, 16);
+    pop();
+    pop();
+  }
+}
+//Pause and Play on click
+function mousePressed() {
+  if (!song1.isPlaying()) {
+    song1.play();
+  } else {
+    song1.pause();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
